8372134bc667e10f4700c1896a01b74c033c3609
Shreyas Gadgin Matha <shreyas.gm61@gmail.com>
2021-04-06 13:07:07 -0400

    Added topn matching

--- src/get_closest_match.py
+++ src/get_closest_match.py
@@ -7,6 +7,7 @@ import nltk
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+from scipy.spatial.distance import cdist, pdist, squareform
 
 
 PROJ = Path(os.path.realpath("."))
@@ -17,12 +18,19 @@ DATA = ROOT / "data"
 def download_resources():
     """Download NLTK and FastText resources required for NLP"""
     import fasttext.util
-    # Download NLTK resources
-    nltk.download("punkt")
-    nltk.download("wordnet")
-    nltk.download("stopwords")
+
+    # Download NLTK resources if they do not exist
+    try:
+        nltk.data.find("tokenizers/punkt")
+        nltk.data.find("corpora/wordnet")
+        nltk.data.find("corpora/stopwords")
+    except LookupError:
+        nltk.download("punkt")
+        nltk.download("wordnet")
+        nltk.download("stopwords")
+
     # Download FastText model
-    fasttext.util.download_model('en', if_exists='ignore')
+    fasttext.util.download_model("en", if_exists="ignore")
 
 
 def process_raw_classification(clas_df, titlecol, codecol=None):
@@ -72,43 +80,52 @@ def preprocess_df(df, text_colname):
         preprocessed_text = " ".join(tokens)
         word_tokenized_corpus = word_punctuation_tokenizer.tokenize(preprocessed_text)
         return word_tokenized_corpus
-    
+
     final_corpus = [preprocess_text(doc) for doc in tqdm(df[text_colname])]
+    assert len(final_corpus) == len(df), "Pre-processing error with some statements"
     return final_corpus
 
-def get_mean_word_vector(doc_text):
-        """Average word vectors to get document vector"""
-        if len(doc_text) > 0:
-            word_vectors = np.array([ft_model.get_word_vector(x) for x in doc_text])
-            result = np.nanmean(word_vectors, axis=0)
-        else:
-            result = np.array([np.nan] * ft_model.get_word_vector("").shape[0])
-        return result
 
-def vectorize_textlist(textlist):
+def get_mean_word_vector(doc_text, ft_model):
+    """Average word vectors to get document vector"""
+    # Calculate word vector and get mean
+    if len(doc_text) > 0:
+        word_vectors = np.array([ft_model.get_word_vector(x) for x in doc_text])
+        result = np.nanmean(word_vectors, axis=0)
+        if result is not None:
+            return result
+
+    # If we get empty result or if input is empty, return nan
+    result = np.array([np.nan] * ft_model.get_word_vector("").shape[0])
+    return result
+
+
+def vectorize_textlist(textlist, ft_model):
     """Convert textlist to embeddings"""
     doc_vectors = np.array(
-        [get_mean_word_vector(x) for x in tqdm(textlist, total=len(textlist))]
+        [get_mean_word_vector(x, ft_model) for x in tqdm(textlist, total=len(textlist))]
     )
     return doc_vectors
 
-def get_embeddings(df, textcol):
+
+def get_embeddings(df, ft_model, textcol):
     """Preprocess text and convert to embeddings"""
     df_text = preprocess_df(df, textcol)
-    vec = vectorize_textlist(df_text)
-    vec_df = pd.concat([df, pd.DataFrame(vec)], axis=1)
+    vec = vectorize_textlist(df_text, ft_model)
+    vec_df = pd.concat([df.reset_index(drop=True), pd.DataFrame(vec)], axis=1)
     vec_df = vec_df.set_index(keys=list(df.columns))
     vec_df.columns = [str(x) for x in vec_df.columns]
     return vec_df
 
+
 def prepare_data_and_embeddings(
-    clas_a_df, clas_b_df, titlecol_a, titlecol_b, codecol_a=None, codecol_b=None
+    clas_a_df, clas_b_df, titlecol_a, titlecol_b, codecol_a, codecol_b
 ):
     print("Pre-processing text")
     # Process raw classification df's
     clas_a_df = process_raw_classification(clas_a_df, titlecol_a, codecol_a)
     clas_b_df = process_raw_classification(clas_b_df, titlecol_b, codecol_b)
-    
+
     # Download required resources
     download_resources()
 
@@ -117,5 +134,47 @@ def prepare_data_and_embeddings(
 
     # Get embeddings
     print("Preparing embeddings")
-    clas_a_vec = get_embeddings(clas_a_df, "clas_a_title")
-    clas_b_vec = get_embeddings(clas_b_df, "clas_b_title")
+    clas_a_vec = get_embeddings(clas_a_df, ft_model, "clas_a_title")
+    clas_b_vec = get_embeddings(clas_b_df, ft_model, "clas_b_title")
+    return clas_a_vec, clas_b_vec
+
+
+def get_topn_matches(clas_a, clas_b, titlecol_b, codecol_b, n_best):
+    """Get top N matches in clas_b for each element in clas_a"""
+    similarity_df = pd.DataFrame(
+        1 - cdist(clas_a.values, clas_b.values, metric="cosine"),
+        index=clas_a.index,
+        columns=clas_b.index,
+    )
+    order = np.argsort(-similarity_df.values, axis=1)[:, :n_best]
+    clas_b_codes = similarity_df.columns.get_level_values(codecol_b)
+    clas_b_names = similarity_df.columns.get_level_values(titlecol_b)
+    result_names = pd.DataFrame(
+        np.array(clas_b_names)[order],
+        columns=["top{}".format(i) for i in range(1, n_best + 1)],
+        index=similarity_df.index,
+    )
+    result_codes = pd.DataFrame(
+        np.array(clas_b_codes)[order],
+        columns=["top{}".format(i) for i in range(1, n_best + 1)],
+        index=similarity_df.index,
+    )
+    return result_codes, result_names
+
+
+def process_data_and_match(
+    clas_a_df,
+    clas_b_df,
+    titlecol_a,
+    titlecol_b,
+    codecol_a=None,
+    codecol_b=None,
+    n_best=5,
+):
+    clas_a_vec, clas_b_vec = prepare_data_and_embeddings(
+        clas_a_df, clas_b_df, titlecol_a, titlecol_b, codecol_a, codecol_b
+    )
+    result_codes, result_names = get_topn_matches(
+        clas_a_vec, clas_b_vec, titlecol_b, codecol_b, n_best
+    )
+    return result_codes, result_names
